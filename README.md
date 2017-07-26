89eab5ccea6d93ed70945643b8407c039fb8abd3
Jason Palmer <jason@jason-palmer.com>
2017-07-26 09:43:31 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # jest-unique-reporter
 A jest reporter that enforces unique test names
+
+## Installation
+
+#### yarn
+```shell
+yarn add --dev jest-unique-reporter
+```
+
+#### npm
+```shell
+npm install --dev jest-unique-reporter
+```
+
+## Usage
+
+#### Add to your jest configuration
+```javascript
+{
+  "reporters": [
+    "<rootDir>/node_modules/jest-unique-reporter"
+  ]
+}
+```
+
+## Configuration
+jest-unique-reporter offers two configuration options.
+
+|field|default|description|
+|---|---|---|
+|global|default=false|if true then test names must be unique across all tests in the project. If false then test names must be unique per file.|
+|useFullName|default=false|if true then describe blocks are included in the test names for comparison. If false then just "it" blocks are used for test names.|
+
+#### Example usage of configuration options
+```javascript
+{
+  "reporters": [
+    ["<rootDir>/node_modules/jest-unique-reporter", {"global": false, "useFullName": false}]
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,94 @@
+// This reporter will keep track of unique test
+// names (optionally with describe blocks)
+// and fail tests if duplicates are found
+
+const chalk = require('chalk');
+
+const rootDir = process.cwd();
+
+const cache = new Set();
+
+class UniqueTestNames {
+  constructor(globalConfig, options) {
+    this._globalConfig = globalConfig;
+    this._options = Object.assign(
+      {},
+      {
+        global: false,
+        useFullName: false
+      },
+      options
+    );
+    this._shouldFail = false;
+    this._duplicates = [];
+  }
+
+  getLastError() {
+    if (this._shouldFail) {
+      console.log(chalk.red(chalk.underline(chalk.bold('Duplicate test names found:'))));
+
+      this._duplicates.forEach((d) => {
+        console.log(chalk.red(chalk.bold('File: '), d.filename));
+        console.log(chalk.red(chalk.bold('Title: '), d.title));
+      });
+      return new Error('Duplicate test names found');
+    }
+
+    return false;
+  }
+
+  onRunComplete(contexts, results) {
+    results.testResults.forEach((suite) => {
+      // Skip over skipped suites
+      if (suite.skipped) return;
+
+      const relativeTestFilePath = suite.testFilePath.replace(rootDir, '');
+
+      const titles = suite.testResults.map((t) => {
+        const testStruct = {
+          title: t.title,
+          filename: relativeTestFilePath
+        };
+
+        let key = '';
+        if (this._options.global) {
+          if (this._options.useFullName) {
+            // Unique describe->test across all files
+            key = t.fullName;
+          }
+
+          // Unique test names across all files
+          key = t.title;
+        } else {
+          if (this._options.useFullName) {
+            // Unique describe->test per file
+            key = `${relativeTestFilePath}-${t.fullName}`;
+          }
+
+          // Unique test name across per file
+          key = `${relativeTestFilePath}-${t.title}`;
+        }
+
+        return Object.assign(
+          {},
+          testStruct,
+          {
+            key: key
+          }
+        );
+      });
+
+      // Check for duplicates
+      titles.forEach((t) => {
+        if (cache.has(t.key)) {
+          this._shouldFail = true;
+          this._duplicates.push(t);
+        }
+
+        cache.add(t.key);
+      });
+    });
+  }
+}
+
+module.exports = UniqueTestNames;
